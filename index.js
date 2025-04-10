d37e2d6980c3f452d639f254963c0fce2c545028
maheskkarande1505 <karandemahesh4@gmail.com>
2025-04-10 11:50:04 +0530

    project structure done
    project structure

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var bodyparser = require("body-parser");
+var upload = require("express-fileupload");
+var session = require("express-session");
+var admin_routes = require("./routes/admin");
+var user_routes = require("./routes/user")
+var app = express();
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(upload());
+app.use(express.static("public/"));
+app.use(session({
+    secret:"maheshkarande",
+    saveUninitialized:true,
+    resave:true
+}));
+
+app.use("/admin",admin_routes);
+app.use("/",user_routes);
+
+app.listen(1000);
