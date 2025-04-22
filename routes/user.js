41c0216f253b34e6ebc1b59f3c94614352dab746
maheskkarande1505 <karandemahesh4@gmail.com>
2025-04-22 12:48:09 +0530

    user site Registration and login done

--- routes/user.js
+++ routes/user.js
@@ -44,6 +44,35 @@ router.get("/do_payment",function(req, res){
 
 router.post("/confirm_order",function(req, res){
     res.redirect("/do_payment")
-})
+});
+
+router.post("/save_account",async function(req, res){
+    var d = req.body;
+    var sql = `INSERT INTO user_accounts (user_name, mobile, email, password) VALUES (?,?,?,?)`;
+    var data = await exe(sql,[d.user_name,d.mobile,d.email,d.password]);
+   // res.send(data);
+   res.send("<script> location.href = document.referrer </script>");
+});
+
+router.post("/proceed_login", async function(req, res){
+    var d = req.body;
+    var sql = `SELECT * FROM user_accounts WHERE email = ? AND password = ?`;
+    var data = await exe(sql,[d.email, d.password]);
+    if(data.length > 0)
+    {
+        var user_id = data[0].user_id;
+        req.session.user_id = user_id;
+       // res.send("login Success");
+       res.send(`
+                    <script> 
+                         var url =  document.referrer;
+                         var new_url = url.replaceAll('?open_login_modal', '');
+                         location.href = new_url;
+                    </script>
+                    `);
+    }
+    else
+    res.send("login Failed");
+});
 
 module.exports = router;
