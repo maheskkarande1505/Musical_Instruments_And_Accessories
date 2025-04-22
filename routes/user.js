e060a64899a07928eb6629f0992e6e961b54d9ad
maheskkarande1505 <karandemahesh4@gmail.com>
2025-04-22 13:11:25 +0530

    login verification done

--- routes/user.js
+++ routes/user.js
@@ -2,48 +2,89 @@ var express = require("express");
 var exe = require("./../connection");
 var router = express.Router()
 
+function verify_login(req, res, next)
+{
+   // req.session.user_id = 1;
+    if(req.session.user_id)
+        next();
+    else
+    res.send("<script> location.href = document.referrer+'?open_login_modal' </script> ");
+}
+
+function logout(req, res) {
+    req.session.destroy(function(err) {
+        if (err) {
+            return res.status(500).send('Failed to log out.');
+        }
+       res.send("<script> location.href = document.referrer+'' </script> "); 
+    });
+}
+
+router.get("/logout", logout, function(req, res){
+   // res.send("<script> location.href = document.referrer+'?open_login_modal' </script> ");
+});
+
+function is_login(req)
+{
+    if(req.session.user_id)
+        return true;
+    else
+        return false;
+}
+
 router.get("/",function(req,res){
-    res.render("user/Home.ejs");
+    var obj = {"is_login":is_login(req)};
+    res.render("user/Home.ejs", obj);
 });
 
 router.get("/about",function(req,res){
-    res.render("user/About.ejs");
+    var obj = {"is_login":is_login(req)};
+    res.render("user/About.ejs",obj);
 });
 
 router.get("/product",function(req,res){
-    res.render("user/Product.ejs");
+    var obj = {"is_login":is_login(req)};
+    res.render("user/Product.ejs",obj);
 });
 
 router.get("/brand",function(req,res){
-    res.render("user/Brand.ejs");
+    var obj = {"is_login":is_login(req)};
+    res.render("user/Brand.ejs",obj);
 });
 
 router.get("/gallery",function(req,res){
-    res.render("user/Gallery.ejs");
+    var obj = {"is_login":is_login(req)};
+    res.render("user/Gallery.ejs",obj);
 });
 
-router.get("/cart",function(req,res){
-    res.render("user/Cart.ejs");
+router.get("/cart",verify_login , function(req,res){
+    var obj = {"is_login":is_login(req)};
+    res.render("user/Cart.ejs",obj);
 });
 
 router.get("/contact",function(req,res){
-    res.render("user/Contact.ejs");
+    var obj = {"is_login":is_login(req)};
+    res.render("user/Contact.ejs",obj);
 });
 
 router.get("/product_details", function(req, res){
-    res.render("user/Product_Details.ejs")
+    var obj = {"is_login":is_login(req)};
+    res.render("user/Product_Details.ejs",obj)
 });
 
-router.get("/checkout",function(req, res){
-    res.render("user/CheckOut.ejs");
+router.get("/checkout", verify_login, function(req, res){
+    var obj = {"is_login":is_login(req)};
+    res.render("user/CheckOut.ejs",obj);
 });
 
-router.get("/do_payment",function(req, res){
-    res.render("user/do_payment.ejs")
+router.get("/do_payment",verify_login, function(req, res){
+    var obj = {"is_login":is_login(req)};
+    res.render("user/do_payment.ejs",obj)
 });
 
 router.post("/confirm_order",function(req, res){
-    res.redirect("/do_payment")
+    var obj = {"is_login":is_login(req)};
+    res.redirect("/do_payment",obj)
 });
 
 router.post("/save_account",async function(req, res){
